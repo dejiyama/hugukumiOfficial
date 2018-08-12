bdeaae21b19afb9ebfdde4831006aa53fb1cdb77
ryohlan <sunsunryohlan@gmail.com>
2018-08-13 07:10:03 +0900

    iOS側をSwift化

---
+++ ios/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  hugukumi_official
+//
+//  Created by Ryohlan on 2018/08/13.
+//  Copyright © 2018年 Facebook. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL
+
+    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index", fallbackResource:nil)
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "hugukumi_official", initialProperties: nil, launchOptions: launchOptions)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+
+    return true
+  }
+}
