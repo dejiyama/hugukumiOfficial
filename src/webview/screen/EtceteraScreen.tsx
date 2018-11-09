5f28cb6fffb55216b4a31ff7fc512c8d5b8150de
dejiyama <deziyama0404@gmail.com>
2018-11-09 11:45:44 +0900

    update:Etcterascreenの戻るボタンを適切なタイミングで表示

--- src/webview/screen/EtceteraScreen.tsx
+++ src/webview/screen/EtceteraScreen.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Image } from 'react-native'
 import { StackNavigator } from 'react-navigation'
 import Item from './EtcScreens/Item'
 import About from './EtcScreens/About'
@@ -10,6 +9,7 @@ import Mypage from './EtcScreens/Mypage'
 import Privacy from './EtcScreens/Privacy'
 import Question from './EtcScreens/Question'
 import Etceteraheader from '../../elements/Etceteraheader'
+
 const Etcetera = StackNavigator(
   {
     Item: { screen: Item },
@@ -28,7 +28,7 @@ const Etcetera = StackNavigator(
           <Etceteraheader
             onClickCart={() => navigation.navigate('Cart')}
             onClickBackpage={() => navigation.goBack()}
-            showBackbutton={navigation.getParam('backButtonEnabled')}
+            showBackbutton={navigation.state.routeName !== 'Item'}
           />
         )
       }
